2edad1619c2a282e4621976b010f02af90cd9b50
maxgol29 <122486753+maxgol29@users.noreply.github.com>
2024-06-06 22:57:20 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+menu = Menu()
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+
+process = True
+
+while process:
+    choice = input(f"What is your choice? {menu.get_items()}: ")
+    if choice == 'off':
+        process = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
+
+
+
